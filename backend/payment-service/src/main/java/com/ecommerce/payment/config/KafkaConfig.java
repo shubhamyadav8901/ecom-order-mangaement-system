640983ad2fd7fee2e0972aaa9899107030dd6420
Shubham Yadav <luckyshubham16@gmail.com>
2026-01-27 01:52:39 +0530

    fix(core): address minor regressions

--- backend/payment-service/src/main/java/com/ecommerce/payment/config/KafkaConfig.java
+++ backend/payment-service/src/main/java/com/ecommerce/payment/config/KafkaConfig.java
@@ -1,4 +1,4 @@
-package com.ecommerce.inventory.config;
+package com.ecommerce.payment.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
